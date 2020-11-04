d50f33ddd2ccb085eabd9c04adb64902cff802c3
leiwx52 <leiwx52@gmail.com>
2020-11-04 10:39:45 +0800

    fix tab

--- datasets.py
+++ datasets.py
@@ -19,19 +19,18 @@ class CelebAHQ(data.Dataset):
         self.num_images = len(self.data)
 
     def preprocess(self):
-        length = len([name for name in os.listdir(self.img_path) if os.path.isfile(os.path.join(self.img_path, name))])
+        length = len([name for name in os.listdir(self.img_path)
+                      if os.path.isfile(os.path.join(self.img_path, name))])
         for i in range(length):
-            img_path = os.path.join(self.img_path, str(i)+ '.jpg')
+            img_path = os.path.join(self.img_path, str(i) + '.jpg')
             self.data.append(img_path)
         xm.master_print('Finished preprocessing the CelebA dataset...')
-
 
     def __getitem__(self, index):
         dataset = self.data
         img_path = dataset[index]
         image = Image.open(img_path).convert('RGB')
         return self.transform_img(image), torch.tensor(0, dtype=torch.long)
-
 
     def __len__(self):
         """Return the number of images."""
@@ -43,55 +42,57 @@ class CelebAHQ(data.Dataset):
         having to load individual images all the time.
     Source code: https://github.com/ajbrock/BigGAN-PyTorch/blob/master/datasets.py
 '''
+
+
 class ILSVRC_HDF5(data.Dataset):
-  def __init__(self, root, transform=None, target_transform=None,
-               load_in_mem=False, train=True,
-               download=False, validate_seed=0,
-               val_split=0, **kwargs): # last four are dummies
+    def __init__(self, root, transform=None, target_transform=None,
+                 load_in_mem=False, train=True,
+                 download=False, validate_seed=0,
+                 val_split=0, **kwargs):  # last four are dummies
 
-    self.root = root
-    self.num_imgs = len(h5.File(root, 'r')['labels'])
+        self.root = root
+        self.num_imgs = len(h5.File(root, 'r')['labels'])
 
-    # self.transform = transform
-    self.target_transform = target_transform
+        # self.transform = transform
+        self.target_transform = target_transform
 
-    # Set the transform here
-    self.transform = transform
+        # Set the transform here
+        self.transform = transform
 
-    # load the entire dataset into memory?
-    self.load_in_mem = load_in_mem
+        # load the entire dataset into memory?
+        self.load_in_mem = load_in_mem
 
-    # If loading into memory, do so now
-    if self.load_in_mem:
-      print('Loading %s into memory...' % root)
-      with h5.File(root,'r') as f:
-        self.data = f['imgs'][:]
-        self.labels = f['labels'][:]
+        # If loading into memory, do so now
+        if self.load_in_mem:
+            print('Loading %s into memory...' % root)
+            with h5.File(root, 'r') as f:
+                self.data = f['imgs'][:]
+                self.labels = f['labels'][:]
 
-  def __getitem__(self, index):
-    """
-    Args:
-        index (int): Index
-    Returns:
-        tuple: (image, target) where target is class_index of the target class.
-    """
-    # If loaded the entire dataset in RAM, get image from memory
-    if self.load_in_mem:
-      img = self.data[index]
-      target = self.labels[index]
+    def __getitem__(self, index):
+        """
+        Args:
+            index (int): Index
+        Returns:
+            tuple: (image, target) where target is class_index of the target class.
+        """
+        # If loaded the entire dataset in RAM, get image from memory
+        if self.load_in_mem:
+            img = self.data[index]
+            target = self.labels[index]
 
-    # Else load it from disk
-    else:
-      with h5.File(self.root,'r') as f:
-        img = f['imgs'][index]
-        target = f['labels'][index]
+        # Else load it from disk
+        else:
+            with h5.File(self.root, 'r') as f:
+                img = f['imgs'][index]
+                target = f['labels'][index]
 
-    img = ((torch.from_numpy(img).float() / 255) - 0.5) * 2
+        img = ((torch.from_numpy(img).float() / 255) - 0.5) * 2
 
-    if self.target_transform is not None:
-      target = self.target_transform(target)
+        if self.target_transform is not None:
+            target = self.target_transform(target)
 
-    return img, int(target)
+        return img, int(target)
 
-  def __len__(self):
-      return self.num_imgs
+    def __len__(self):
+        return self.num_imgs
--- train_fns.py
+++ train_fns.py
@@ -14,188 +14,224 @@ import logging
 
 logging.basicConfig(level=logging.INFO)
 
+
 def master_log(s):
     if xm.is_master_ordinal():
         logging.log(logging.INFO, s)
 
 # Dummy training function for debugging
+
+
 def dummy_training_function():
-  def train(x, y):
-    return {}
-  return train
+    def train(x, y):
+        return {}
+    return train
 
 
 def GAN_training_function(G, D, GD, sample, ema, state_dict, config):
-  def train(x, y):
-    xm.mark_step()
-    G.optim.zero_grad()
-    D.optim.zero_grad()
-    # How many chunks to split x and y into?
-    x = torch.split(x, config['batch_size'])
-    y = torch.split(y, config['batch_size'])
-    counter = 0
-
-    # Optionally toggle D and G's "require_grad"
-    if config['toggle_grads']:
-      utils.toggle_grad(D, True)
-      utils.toggle_grad(G, False)
-
-    for step_index in range(config['num_D_steps']):
-      # If accumulating gradients, loop multiple times before an optimizer step
-      xm.mark_step()
-      D.optim.zero_grad()
-      
-      for accumulation_index in range(config['num_D_accumulations']):
+    def train(x, y):
         xm.mark_step()
-        z_, y_ = sample()
-        D_fake, D_real = GD(z_[:config['batch_size']], y_[:config['batch_size']],
-                            x[counter], y[counter], train_G=False,
-                            split_D=config['split_D'])
-        # Compute components of D's loss, average them, and divide by
-        # the number of gradient accumulations
-        D_loss_real, D_loss_fake = losses.discriminator_loss(D_fake, D_real)
-        D_loss = (D_loss_real + D_loss_fake) / float(config['num_D_accumulations'])
-        D_loss.backward()
-        counter += 1
+        G.optim.zero_grad()
+        D.optim.zero_grad()
+        # How many chunks to split x and y into?
+        x = torch.split(x, config['batch_size'])
+        y = torch.split(y, config['batch_size'])
+        counter = 0
 
-      # Optionally apply ortho reg in D
-      if config['D_ortho'] > 0.0:
-        # Debug print to indicate we're using ortho reg in D.
-        xm.master_print('using modified ortho reg in D')
+        # Optionally toggle D and G's "require_grad"
+        if config['toggle_grads']:
+            utils.toggle_grad(D, True)
+            utils.toggle_grad(G, False)
+
+        for step_index in range(config['num_D_steps']):
+            # If accumulating gradients, loop multiple times before an
+            # optimizer step
+            xm.mark_step()
+            D.optim.zero_grad()
+
+            for accumulation_index in range(config['num_D_accumulations']):
+                xm.mark_step()
+                z_, y_ = sample()
+                D_fake, D_real = GD(z_[:config['batch_size']], y_[:config['batch_size']],
+                                    x[counter], y[counter], train_G=False,
+                                    split_D=config['split_D'])
+                # Compute components of D's loss, average them, and divide by
+                # the number of gradient accumulations
+                D_loss_real, D_loss_fake = losses.discriminator_loss(
+                    D_fake, D_real)
+                D_loss = (D_loss_real + D_loss_fake) / \
+                    float(config['num_D_accumulations'])
+                D_loss.backward()
+                counter += 1
+
+            # Optionally apply ortho reg in D
+            if config['D_ortho'] > 0.0:
+                # Debug print to indicate we're using ortho reg in D.
+                xm.master_print('using modified ortho reg in D')
+                xm.mark_step()
+                utils.ortho(D, config['D_ortho'])
+
+            xm.optimizer_step(D.optim)
+
+        # Optionally toggle "requires_grad"
+        if config['toggle_grads']:
+            utils.toggle_grad(D, False)
+            utils.toggle_grad(G, True)
+
+        # Zero G's gradients by default before training G, for safety
         xm.mark_step()
-        utils.ortho(D, config['D_ortho'])
+        G.optim.zero_grad()
 
-      xm.optimizer_step(D.optim)
+        # If accumulating gradients, loop multiple times
+        for accumulation_index in range(config['num_G_accumulations']):
+            xm.mark_step()
+            z_, y_ = sample()
+            D_fake = GD(z_, y_, train_G=True, split_D=config['split_D'])
+            G_loss = losses.generator_loss(
+                D_fake) / float(config['num_G_accumulations'])
+            G_loss.backward()
 
-    # Optionally toggle "requires_grad"
-    if config['toggle_grads']:
-      utils.toggle_grad(D, False)
-      utils.toggle_grad(G, True)
+        # Optionally apply modified ortho reg in G
+        if config['G_ortho'] > 0.0:
+            # Debug print to indicate we're using ortho reg in G
+            print('using modified ortho reg in G')
+            # Don't ortho reg shared, it makes no sense. Really we should
+            # blacklist any embeddings for this
+            xm.mark_step()
+            utils.ortho(G, config['G_ortho'],
+                        blacklist=[param for param in G.shared.parameters()])
+        xm.mark_step()
+        xm.optimizer_step(G.optim)
 
-    # Zero G's gradients by default before training G, for safety
-    xm.mark_step()
-    G.optim.zero_grad()
+        # If we have an ema, update it, regardless of if we test with it or not
+        if config['ema']:
+            ema.update(state_dict['itr'])
 
-    # If accumulating gradients, loop multiple times
-    for accumulation_index in range(config['num_G_accumulations']):
-      xm.mark_step()
-      z_, y_ = sample()
-      D_fake = GD(z_, y_, train_G=True, split_D=config['split_D'])
-      G_loss = losses.generator_loss(D_fake) / float(config['num_G_accumulations'])
-      G_loss.backward()
+        out = {'G_loss': G_loss,
+               'D_loss_real': D_loss_real,
+               'D_loss_fake': D_loss_fake}
+        # Return G's loss and the components of D's loss.
+        return out
+    return train
 
-    # Optionally apply modified ortho reg in G
-    if config['G_ortho'] > 0.0:
-      print('using modified ortho reg in G') # Debug print to indicate we're using ortho reg in G
-      # Don't ortho reg shared, it makes no sense. Really we should blacklist any embeddings for this
-      xm.mark_step()
-      utils.ortho(G, config['G_ortho'],
-                  blacklist=[param for param in G.shared.parameters()])
-    xm.mark_step()
-    xm.optimizer_step(G.optim)
-
-    # If we have an ema, update it, regardless of if we test with it or not
-    if config['ema']:
-      ema.update(state_dict['itr'])
-
-    out = {'G_loss': G_loss,
-            'D_loss_real': D_loss_real,
-            'D_loss_fake': D_loss_fake}
-    # Return G's loss and the components of D's loss.
-    return out
-  return train
 
 ''' This function takes in the model, saves the weights (multiple copies if
     requested), and prepares sample sheets: one consisting of samples given
     a fixed noise seed (to show how the model evolves throughout training),
     a set of full conditional sample sheets, and a set of interp sheets. '''
+
+
 def save_and_sample(G, D, G_ema, sample, fixed_z, fixed_y,
                     state_dict, config, experiment_name):
-  utils.save_weights(G, D, state_dict, config['weights_root'],
-                     experiment_name, None, G_ema if config['ema'] else None)
-  # Save an additional copy to mitigate accidental corruption if process
-  # is killed during a save (it's happened to me before -.-)
-  if config['num_save_copies'] > 0:
     utils.save_weights(G, D, state_dict, config['weights_root'],
-                       experiment_name,
-                       'copy%d' %  state_dict['save_num'],
-                       G_ema if config['ema'] else None)
-    state_dict['save_num'] = (state_dict['save_num'] + 1 ) % config['num_save_copies']
+                       experiment_name, None, G_ema if config['ema'] else None)
+    # Save an additional copy to mitigate accidental corruption if process
+    # is killed during a save (it's happened to me before -.-)
+    if config['num_save_copies'] > 0:
+        utils.save_weights(G, D, state_dict, config['weights_root'],
+                           experiment_name,
+                           'copy%d' % state_dict['save_num'],
+                           G_ema if config['ema'] else None)
+        state_dict['save_num'] = (
+            state_dict['save_num'] + 1) % config['num_save_copies']
 
-  # Use EMA G for samples or non-EMA?
-  which_G = G_ema if config['ema'] and config['use_ema'] and (state_dict['itr'] > config['ema_start']) else G
+    # Use EMA G for samples or non-EMA?
+    which_G = G_ema if config['ema'] and config['use_ema'] and (
+        state_dict['itr'] > config['ema_start']) else G
 
+    # Accumulate standing statistics?
+    if config['accumulate_stats']:
+        utils.accumulate_standing_stats(
+            G_ema if config['ema'] and config['use_ema'] else G,
+            sample, config['n_classes'], config['num_standing_accumulations'])
 
-  # Accumulate standing statistics?
-  if config['accumulate_stats']:
-    utils.accumulate_standing_stats(
-        G_ema if config['ema'] and config['use_ema'] else G,
-        sample, config['n_classes'], config['num_standing_accumulations'])
-
-  # Save a random sample sheet with fixed z and y
-  with torch.no_grad():
-      fixed_Gz = which_G(fixed_z, which_G.shared(fixed_y))
-  if not os.path.isdir('%s/%s' % (config['samples_root'], experiment_name)):
-      try:
-        os.mkdir('%s/%s' % (config['samples_root'], experiment_name))
-      except:
-          pass
-  image_filename = '%s/%s/fixed_samples%d.jpg' % (config['samples_root'],
-                                                  experiment_name,
-                                                  state_dict['itr'])
-  torchvision.utils.save_image(fixed_Gz.float().cpu(), image_filename,
-                             nrow=int(fixed_Gz.shape[0] **0.5), normalize=True)
-  # For now, every time we save, also save sample sheets
-  #utils.sample_sheet(which_G,
-  #                   classes_per_sheet=utils.classes_per_sheet_dict[config['dataset']],
-  #                   num_classes=config['n_classes'],
-  #                   samples_per_class=5, parallel=config['parallel'],
-  #                   samples_root=config['samples_root'],
-  #                   experiment_name=experiment_name,
-  #                   folder_number=state_dict['itr'],
-  #                   z_=None)
-  # Also save interp sheets
-  #for fix_z, fix_y in zip([False, False, True], [False, True, False]):
-  #  utils.interp_sheet(which_G,
-  #                     num_per_sheet=16,
-  #                     num_midpoints=8,
-  #                     num_classes=config['n_classes'],
-  #                     parallel=config['parallel'],
-  #                     samples_root=config['samples_root'],
-  #                     experiment_name=experiment_name,
-  #                     folder_number=state_dict['itr'],
-  #                     sheet_number=0,
-  #                     fix_z=fix_z, fix_y=fix_y)
-
+    # Save a random sample sheet with fixed z and y
+    with torch.no_grad():
+        fixed_Gz = which_G(fixed_z, which_G.shared(fixed_y))
+    if not os.path.isdir('%s/%s' % (config['samples_root'], experiment_name)):
+        try:
+            os.mkdir('%s/%s' % (config['samples_root'], experiment_name))
+        except BaseException:
+            pass
+    image_filename = '%s/%s/fixed_samples%d.jpg' % (config['samples_root'],
+                                                    experiment_name,
+                                                    state_dict['itr'])
+    torchvision.utils.save_image(
+        fixed_Gz.float().cpu(), image_filename, nrow=int(
+            fixed_Gz.shape[0] ** 0.5), normalize=True)
+    # For now, every time we save, also save sample sheets
+    # utils.sample_sheet(which_G,
+    #                   classes_per_sheet=utils.classes_per_sheet_dict[config['dataset']],
+    #                   num_classes=config['n_classes'],
+    #                   samples_per_class=5, parallel=config['parallel'],
+    #                   samples_root=config['samples_root'],
+    #                   experiment_name=experiment_name,
+    #                   folder_number=state_dict['itr'],
+    #                   z_=None)
+    # Also save interp sheets
+    # for fix_z, fix_y in zip([False, False, True], [False, True, False]):
+    #  utils.interp_sheet(which_G,
+    #                     num_per_sheet=16,
+    #                     num_midpoints=8,
+    #                     num_classes=config['n_classes'],
+    #                     parallel=config['parallel'],
+    #                     samples_root=config['samples_root'],
+    #                     experiment_name=experiment_name,
+    #                     folder_number=state_dict['itr'],
+    #                     sheet_number=0,
+    #                     fix_z=fix_z, fix_y=fix_y)
 
 
 ''' This function runs the inception metrics code, checks if the results
     are an improvement over the previous best (either in IS or FID,
     user-specified), logs the results, and saves a best_ copy if it's an
     improvement. '''
-def test(G, D, G_ema, sample, state_dict, config, model_sample, get_inception_metrics,
-         experiment_name, test_log):
-  master_log('Gathering inception metrics...')
-  if config['accumulate_stats']:
-    utils.accumulate_standing_stats(G_ema if config['ema'] and config['use_ema'] else G,
-                           sample, config['n_classes'],
-                           config['num_standing_accumulations'])
-  IS_mean, IS_std, FID = get_inception_metrics(model_sample,
-                                               config['num_inception_images'],
-                                               num_splits=10)
-  master_log('Itr %d: PYTORCH UNOFFICIAL Inception Score is %3.3f +/- %3.3f, PYTORCH UNOFFICIAL FID is %5.4f' % (state_dict['itr'], IS_mean, IS_std, FID))
-  # If improved over previous best metric, save approrpiate copy
-  if ((config['which_best'] == 'IS' and IS_mean > state_dict['best_IS'])
-    or (config['which_best'] == 'FID' and FID < state_dict['best_FID'])):
-    master_log('%s improved over previous best, saving checkpoint...' % config['which_best'])
-    utils.save_weights(G, D, state_dict, config['weights_root'],
-                       experiment_name, 'best%d' % state_dict['save_best_num'],
-                       G_ema if config['ema'] else None)
-    state_dict['save_best_num'] = (state_dict['save_best_num'] + 1 ) % config['num_best_copies']
-  state_dict['best_IS'] = max(state_dict['best_IS'], IS_mean)
-  state_dict['best_FID'] = min(state_dict['best_FID'], FID)
 
-  if xm.is_master_ordinal():
-      # Log results to file
-      test_log.log(itr=int(state_dict['itr']), IS_mean=float(IS_mean),
-                   IS_std=float(IS_std), FID=float(FID))
+
+def test(
+        G,
+        D,
+        G_ema,
+        sample,
+        state_dict,
+        config,
+        model_sample,
+        get_inception_metrics,
+        experiment_name,
+        test_log):
+    master_log('Gathering inception metrics...')
+    if config['accumulate_stats']:
+        utils.accumulate_standing_stats(
+            G_ema if config['ema'] and config['use_ema'] else G,
+            sample,
+            config['n_classes'],
+            config['num_standing_accumulations'])
+    IS_mean, IS_std, FID = get_inception_metrics(
+        model_sample, config['num_inception_images'], num_splits=10)
+    master_log(
+        'Itr %d: PYTORCH UNOFFICIAL Inception Score is %3.3f +/- %3.3f, PYTORCH UNOFFICIAL FID is %5.4f' %
+        (state_dict['itr'], IS_mean, IS_std, FID))
+    # If improved over previous best metric, save approrpiate copy
+    if ((config['which_best'] == 'IS' and IS_mean > state_dict['best_IS']) or (
+            config['which_best'] == 'FID' and FID < state_dict['best_FID'])):
+        master_log(
+            '%s improved over previous best, saving checkpoint...' %
+            config['which_best'])
+        utils.save_weights(
+            G,
+            D,
+            state_dict,
+            config['weights_root'],
+            experiment_name,
+            'best%d' %
+            state_dict['save_best_num'],
+            G_ema if config['ema'] else None)
+        state_dict['save_best_num'] = (
+            state_dict['save_best_num'] + 1) % config['num_best_copies']
+    state_dict['best_IS'] = max(state_dict['best_IS'], IS_mean)
+    state_dict['best_FID'] = min(state_dict['best_FID'], FID)
+
+    if xm.is_master_ordinal():
+        # Log results to file
+        test_log.log(itr=int(state_dict['itr']), IS_mean=float(IS_mean),
+                     IS_std=float(IS_std), FID=float(FID))
